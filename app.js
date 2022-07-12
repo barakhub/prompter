1cfcde9c925960abf3b6936793ee8f1915736187
Barak Ori <barakori@gmail.com>
2022-07-12 11:34:53 +0300

    Showing markdown from file

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+const yargs = require('yargs')
 const express = require('express')
-const app = express()
-const port = 3000
+const fs = require('fs/promises');
+var markdownIt = require('markdown-it')({breaks: true});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express()
+
+const argv = yargs
+    .option('port', {
+      alias: 'p',
+      description: 'Server port',
+      type: 'number',
+      default: 3000
+    })
+    .option('file', {
+      alias: 'f',
+      description: 'File where the content is stored',
+      default: '/Users/barakori/dev/prompter/sample.md',
+    })
+    .help().alias('help', 'h').argv;
+
+const port = argv.port
+
+const fileLocation = '/home'
+
+app.get('/', async (req, res) => {
+  const markdown = await loadFile();
+  if (markdown) {
+    const html = markdownIt.render(markdown);
+    res.send(html);
+  } else {
+    res.send('file not found');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+async function loadFile() {
+  try {
+    const data = await fs.readFile(argv.file, { encoding: 'utf8' });
+    return data;
+  } catch(error) {
+    console.info('Could not load file: ' + error)
+    return null;
+  }
+}
